b6208d32f50f6f6cf15541d427e696f1a2a0dbd3
Ankush Choudhary <ankush282000@gmail.com>
2024-11-09 15:20:15 +0530

    get all blood request update

--- src/routes/requestRoutes.ts
+++ src/routes/requestRoutes.ts
@@ -4,5 +4,6 @@ import { BloodRequestController } from "../controllers/requestController";
 const router = Router();
 
 router.route("/blood-requests").post(BloodRequestController.createBloodRequest).get(BloodRequestController.getAllBloodRequests);
+router.route("/requests").post(BloodRequestController.getAllBloodRequests);
 
 export default router;
